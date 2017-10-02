a4b417d3f1c1b9263b5d782b5fdeffc831e4cf4c
jakubnowicki <q.nowicki@gmail.com>
2017-10-02 14:35:56 +0200

    bgfx

--- R/find_missing_landmarks.R
+++ R/find_missing_landmarks.R
@@ -6,12 +6,13 @@
 #' @param method clustering method
 #' @param clear.names optional bolean for clearing specimens IDs; default = FALSE
 #' @param split optional split pattern for clearing IDs (directory mark); default = '\\'
+#' @param clust.parameter minimal proportion of missed landmarks in 'clust method; deafult = 0.45
 #' @export
 
-find.missing.landmarks <- function(data,method = 'clust',clear.names = FALSE, split = '\\') {
+find.missing.landmarks <- function(data,method = 'clust',clear.names = FALSE, split = '\\', clust.parameter = 0.45) {
     n.specimens <- dim(data)[3]
     for (i in 1:n.specimens) {
-        data[,,i] <- missing.landmarks(data[,,i],method = method)
+        data[,,i] <- missing.landmarks(data[,,i],method = method, clust.parameter = clust.parameter)
     }
     if (clear.names) {
         data <- clear.specimens.names(data,split = split)
