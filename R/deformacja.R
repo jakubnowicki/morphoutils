1fb345d24ac7999a56851d3ec9530d3547539fe9
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2018-01-05 22:28:38 +0100

    Deformation with different angles

--- R/deformacja.R
+++ R/deformacja.R
@@ -6,7 +6,14 @@
 #' @param strain.matrix Strain matrix
 #' @export
 
-deformacja <- function(data,strain.matrix) {
-  wynik <- strain.matrix %*% t(data)
+deformacja <- function(data,strain.matrix, angle = NULL) {
+    if (!is.null(angle)) {
+        angle.matrix <- matrix(data = c(cos(angle), sin(angle),-sin(angle),cos(angle)), nrow = 2,ncol = 2)
+        wynik <- angle.matrix %*% t(data)
+        wynik <- t(wynik)
+    } else {
+        wynik <- data
+    }
+  wynik <- strain.matrix %*% t(wynik)
   return(t(wynik))
 }
