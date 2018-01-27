abbb2e19b9dac96257e22a4e10fb31fd1705dddf
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2018-01-27 19:05:03 +0100

    strain matrix upgrade

--- R/strain_matrix.R
+++ R/strain_matrix.R
@@ -3,12 +3,17 @@
 #' Generates strain matrix
 #' @param a a
 #' @param theta theta
+#' @param type as in Angielczyk and Sheets (2007) = 1, or with -sin(theta) = 2
 #' @export
 
-strain.matrix <- function(a, theta) {
+strain.matrix <- function(a, theta, type = 1) {
   wynik<-matrix(0,ncol=2,nrow=2)
   wynik[1,1]<-a*cos(theta)
-  wynik[1,2]<-sin(theta)
+  if (type == 1) {
+      wynik[1,2]<-sin(theta)
+  } else {
+      wynik[1,2] <- -sin(theta)
+  }
   wynik[2,1]<-sin(theta)
   wynik[2,2]<-(1/a)*cos(theta)
   return(wynik)
